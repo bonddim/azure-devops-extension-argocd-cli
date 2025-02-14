08dba04ce3c72efc5af51957e488a68515fe30dd
Dmytro Bondar <git@bonddim.dev>
2025-02-14 15:30:58 +0100

    Init extension

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
 # Argo CD Task For Azure DevOps Pipelines
+
+This extension installs the [Argo CD CLI](https://argo-cd.readthedocs.io/en/stable/) on an Azure DevOps agent.
+It automates the installation process and configures the environment for seamless integration with your pipeline.
+
+## Features
+
+- Installs the latest [released](https://github.com/argoproj/argo-cd/releases) version by default.
+- Supports Linux, macOS, and Windows agents.
+- Creates an **Argo CD Server** service connection to securely store credentials.
+- Sets **ARGOCD_SERVER** and **ARGOCD_AUTH_TOKEN** environment variables from the provided service connection.
+- Optionally sets the **ARGOCD_OPTS** variable for extra configuration.
+
+## Installation
+
+Install the extension from the [Azure DevOps Marketplace](https://marketplace.visualstudio.com/items/bonddim.argocd-installer).
+
+## Usage
+
+### Install Latest Version
+
+Use this configuration to install the latest released version:
+
+```yaml
+- task: ArgoCDInstaller@0
+# or
+- task: ArgoCDInstaller@0
+  inputs:
+    version: latest
+```
+
+### Install specific version
+
+To install a specific version of Argo CD CLI, specify the desired version
+
+```yaml
+- task: ArgoCDInstaller@0
+  inputs:
+    version: v2.14.2
+```
+
+### Install server version
+
+This option installs the version matching your server and also sets `ARGOCD_OPTS="--grpc-web"`.
+
+```yaml
+- task: ArgoCDInstaller@0
+  inputs:
+    connection: ServiceConnectionName or ServiceConnectionID
+    version: server
+    options: --grpc-web
+```
+
+## Contributing
+
+Contributions are welcome! Please review our contributing guidelines before submitting a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Additional Information
+
+For detailed documentation, FAQs, or troubleshooting tips, please refer to the [Argo CD documentation](https://argo-cd.readthedocs.io/en/stable/).
---
+++ installer/index.ts
@@ -0,0 +1,126 @@
+import * as tool from "azure-pipelines-tool-lib/tool";
+import * as task from "azure-pipelines-task-lib/task";
+import * as os from "os";
+import * as fs from "fs";
+
+const FallbackVersion = "v2.14.1";
+const ReleaseUrl = "https://github.com/argoproj/argo-cd/releases";
+const ToolName = "argocd";
+let ServerUrl = "";
+
+const fileExtension = os.platform() === "win32" ? ".exe" : "";
+
+async function run() {
+  try {
+    getEndpointDetails();
+    const inputVersion: string | undefined = task.getInput('version') ?? "latest";
+    const inputOpts: string | undefined = task.getInput('options');
+    if (inputOpts) {
+      task.setVariable("ARGOCD_OPTS", inputOpts);
+    }
+
+    const versionSpec = await resolveVersion(inputVersion);
+    const toolPath = tool.findLocalTool(ToolName, versionSpec);
+    if (!toolPath) {
+      await installCli(versionSpec);
+    } else {
+      tool.prependPath(toolPath);
+    }
+    task.execSync(ToolName, "version --client");
+
+  } catch (err: any) {
+    task.setResult(task.TaskResult.Failed, err.message);
+  }
+}
+
+function getEndpointDetails() {
+  const endpoint = task.getInput("connection", false);
+
+  if (!endpoint) {
+    task.debug("No service connection provided");
+    return;
+  }
+
+  ServerUrl = task.getEndpointUrlRequired(endpoint);
+  const apitoken = task.getEndpointAuthorizationParameterRequired(endpoint, "apitoken");
+  const url = new URL(ServerUrl);
+  task.setVariable("ARGOCD_SERVER", url.host + (url.pathname !== "/" ? url.pathname : ""));
+  task.setVariable("ARGOCD_AUTH_TOKEN", apitoken);
+}
+
+async function resolveVersion(inputVersion: string): Promise<string> {
+  task.debug(`Requested ${inputVersion} version...`);
+
+  switch (inputVersion.toLowerCase()) {
+  case "latest":
+    return resolveLatest(ReleaseUrl);
+  case "server":
+    return resolveServer(ServerUrl);
+  default:
+    return inputVersion;
+  }
+}
+
+async function resolveLatest(url: string): Promise<string> {
+  const response = await fetch(new Request(`${url}/latest`, { redirect: "manual" }));
+  const locationHeader = response.headers.get("location");
+  const parts = locationHeader?.split("/");
+  const version = parts?.pop();
+
+  if (!version) {
+    task.warning(`Failed to resolve version. Using fallback version ${FallbackVersion}`);
+    return FallbackVersion;
+  }
+
+  task.debug(`Resolved ${version} version`);
+  return version;
+}
+
+async function resolveServer(url: string): Promise<string> {
+  const response = await fetch(new Request(`${url}/api/version`));
+  const data = await response.json();
+  const versionFull = data.Version;
+
+  if (!versionFull) {
+    task.warning(`Failed to resolve version. Using fallback version ${FallbackVersion}`);
+    return FallbackVersion;
+  }
+
+  const version = versionFull.split("+")[0];
+  task.debug(`Resolved ${version} version`);
+  return version;
+}
+
+async function installCli(version: string) {
+  const downloadUrl = await getDownloadUrl(version);
+  const tempFilePath = await tool.downloadTool(downloadUrl);
+
+  if (os.platform() !== "win32") {
+    fs.chmodSync(tempFilePath, "0755");
+  }
+
+  const cachedPath = await tool.cacheFile(tempFilePath, ToolName + fileExtension, ToolName, version);
+  task.debug("Cached tool at: " + cachedPath);
+  tool.prependPath(cachedPath);
+}
+
+async function getDownloadUrl(version: string): Promise<string> {
+  const currentArch = os.arch() === 'x64' ? 'amd64' : os.arch();
+  const currentPlatform = os.platform() === 'win32' ? 'windows' : os.platform();
+  let supportedArchs: string[] = [];
+
+  if (currentPlatform === "windows") {
+    supportedArchs = ["amd64"];
+  } else if (currentPlatform === "darwin" || currentPlatform === "linux") {
+    supportedArchs = ["amd64", "arm64"];
+  } else {
+    throw new Error(`Unsupported platform: ${currentPlatform}`);
+  }
+
+  if (!supportedArchs.includes(currentArch)) {
+    throw new Error(`Unsupported architecture for ${currentPlatform}: ${currentArch}`);
+  }
+  return `${ReleaseUrl}/download/${version}/${ToolName}-${currentPlatform}-${currentArch}${fileExtension}`;
+}
+
+run();
